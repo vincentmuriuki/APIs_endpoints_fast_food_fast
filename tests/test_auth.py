93b7ff78f451cc1225500c7b0a2cd35b7174d433
Vincent <vincentmuriuki42@gmail.com>
2018-09-07 22:21:07 +0300

    Update .travis.yml

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -17,19 +17,6 @@ class TestAuth(EntryClass):
         self.assertEqual(result["message"],
                          "registration successful, now login")
 
-    def test_user_wrong_registration(self):
-        """Test wrong registration when user doesn't fill fields"""
-        response = self.client.post(SIGNUP_URL,
-
-                                    data=json.dumps(
-
-                                        {'username': 'vinc', 'email': 'vinc@mail.com', 'pass': ''}),
-
-                                    content_type='application/json')
-
-        self.assertEqual(response.status_code, 400)
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["message"], "All fields are required")
 
     def test_max_reg(self):
         """ Test User cannot register twice """
@@ -55,27 +42,6 @@ class TestAuth(EntryClass):
         self.assertEqual(result["message"],
                          "Password should be atleast 8 characters")
 
-    def test_len_of_username(self):
-        """ Username shhould not be less than 4 charcters """
-        response = self.client.post(SIGNUP_URL,
-                                    data=json.dumps(
-                                        {'username': 'kash', 'email': 'kash@gmail.com', 'password': 'password12'}),
-                                    content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["message"],
-                         "Username should be atleast 4 characters")
-
-    def test_validity_of_email(self):
-        """ Test user should not be able to register with invalid email """
-        response = self.client.post(SIGNUP_URL,
-                                    data=json.dumps(
-                                        {'username': 'vincent', 'email': 'vinc', 'password': '1234567890'}),
-                                    content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-        result = json.loads(response.data.decode())
-        self.assertEqual(
-            result["message"], "Check your email. It should be of the form name@mail.com")
 
     def test_username_validity(self):
         response = self.client.post(SIGNUP_URL,
@@ -85,35 +51,3 @@ class TestAuth(EntryClass):
         self.assertEqual(response.status_code, 400)
         result = json.loads(response.data.decode())
         self.assertEqual(result["message"], "Invalid username")
-
-    def test_login(self):
-        self.test_user.save()
-        response = self.client.post(LOGIN_URL,
-                                    data=json.dumps(
-                                        {'username': 'kash', 'password': '123456789'}),
-                                    content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        result = json.loads(response.data.decode())
-        self.assertEqual(result["message"], "You have successfully logged in")
-
-    def test_if_user_exists(self):
-        response = self.client.post(LOGIN_URL,
-                                    data=json.dumps(
-                                        {'username': 'mukami', 'password': 'mukash'}),
-                                    content_type='application/json')
-        self.assertEqual(response.status_code, 404)
-        result = json.loads(response.data.decode())
-        self.assertEqual(result['message'], 'User does not exist. Create a new account!')
-
-    def test_wrong_password(self):
-        """Test for authenication when password is wrong
-        User should not be able to login
-        """
-        self.test_user.save()
-        response = self.client.post(LOGIN_URL,
-                                    data=json.dumps(
-                                        {'username': 'dannyb', 'password': 'andela'}),
-                                    content_type='application/json')
-        self.assertEqual(response.status_code, 401)
-        result = json.loads(response.data.decode())
-        self.assertEqual(result['message'], 'Username or password is wrong.')
