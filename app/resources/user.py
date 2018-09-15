874b5b578d2821b6da3e6c6b5deacbdbb4705275
Vincent <vincentmuriuki42@gmail.com>
2018-09-16 00:33:37 +0300

    Modifications

--- app/resources/user.py
+++ app/resources/user.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask_restful import Resource, reqparse
 import re
 
@@ -17,34 +17,32 @@ class Signup(Resource):
                         help='Please input the email', type=str)
     parser.add_argument('password', required=True,
                         help='Please imput the password', type=str)
-
+       """ Registering new user """
     def post(self):
-        """ Registering new user """
         args = Signup.parser.parse_args()
         password = args.get('password')
         username = args.get('username')
         email = args.get('email')
-
         email_format = re.compile(
             r"(^[a-zA-Z0-9_.-]+@[a-zA-Z-]+\.[a-zA-Z-]+$)")
         username_format = re.compile(r"(^[A-Za-z]+$)")
-
         if not (re.match(username_format, username)):
-            return {'message': 'Invalid username'}, 400
+            # return make_response('message': 'Invalid username'), 400
+            return make_response('Username not recgnized! Please Try again!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required!"'})
+
         elif not (re.match(email_format, email)):
-            return {'message': 'Invalid email. Ensure email is of the form example@mail.com'}, 400
-        if len(username) < 4:
-            return {'message': 'Username should be atleast 4 characters'}, 400
+            return make_response{'message': 'Invalid email. Ensure email is of the form example@mail.com'}, 400
         if is_blank(password) or is_blank(username) or is_blank(email):
-            return {'message': 'All fields are required'}, 400
-        if len(password) < 8:
-            return {'message': 'Password should be atleast 8 characters'}, 400
+            # return 'message': 'All fields are required'}, 400
+            return make_response('All fields are required! Please Try again!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required!"'})
 
         username_exists = User.get_user_by_username(username=args['username'])
         email_exists = User.get_user_by_email(email=args['email'])
-
         if username_exists or email_exists:
-            return {'message': 'User already exists'}, 203
+            # return {'message': 'User already exists'}, 203
+            return make_response('User already exists in the database! Please Try a different username!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required!"'})
+
+
 
         user = User(username=args.get('username'),
                     email=args.get('email'), password=password)
